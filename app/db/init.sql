080612f8a57a367e69169f5032164e28764fb0eb
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-05-30 21:29:56 +0300

    Add SQL script for db creation

---
+++ app/db/init.sql
@@ -0,0 +1,17 @@
+CREATE DATABASE members_only;
+
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    email VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    first_name VARCHAR(255),
+    last_name VARCHAR(255),
+    admin BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE posts (
+    id SERIAL PRIMARY KEY,
+    content TEXT,
+    release_date DATE,
+    user_id INTEGER REFERENCES users(id) ON DELETE SET NULL
+);
