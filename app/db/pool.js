ef02e954dccb9dd6b8f10141320dbf1d36d4d1b1
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-06-01 20:56:08 +0300

    Use connection string for pool

--- app/db/pool.js
+++ app/db/pool.js
@@ -3,9 +3,8 @@ const { Pool } = require("pg");
 require("dotenv").config({ path: "../.env" });
 
 module.exports = new Pool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  database: process.env.DB_DATABASE,
-  password: process.env.DB_PASSWORD,
-  port: process.env.DB_PORT,
+  connectionString: process.env.DB_CONNECTION_STRING,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
