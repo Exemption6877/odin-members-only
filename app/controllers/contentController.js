dc7e2548965a8c65d2539e331ee16ddb39e5df59
Exemption6877 <106035951+Exemption6877@users.noreply.github.com>
2025-06-01 19:16:42 +0300

    Utilize date-fns for date handling in messages

--- app/controllers/contentController.js
+++ app/controllers/contentController.js
@@ -1,9 +1,18 @@
 const db = require("../db/query");
+const { formatRelative } = require("date-fns");
 
 async function getGuestContent(req, res) {
   try {
     const messages = await db.getAllPosts();
-    res.render("index", { messages: messages });
+    const formattedMessages = messages.map((message) => {
+      const formattedDate = formatRelative(
+        new Date(message.release_date),
+        new Date()
+      );
+      return { ...message, release_date: formattedDate };
+    });
+
+    res.render("index", { messages: formattedMessages });
   } catch (err) {
     res.status(500).send("Something went wrong");
   }
